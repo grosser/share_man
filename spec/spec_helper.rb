8c5692a439383c805c5621f0667f098c0594ca5e
grosser <grosser.michael@gmail.com>
2012-01-09 06:33:13 -0800

    spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,26 @@
+require 'rubygems'
+require 'spork'
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  if ENV["RUBYMINE_HOME"]
+    puts "Rubymine support"
+    $:.unshift(File.expand_path("rb/testing/patch/common", ENV["RUBYMINE_HOME"]))
+    $:.unshift(File.expand_path("rb/testing/patch/bdd", ENV["RUBYMINE_HOME"]))
+  end
 
-RSpec.configure do |config|
-  config.mock_with :rspec
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+    config.render_views
+  end
+end
+
+Spork.each_run do
 end
